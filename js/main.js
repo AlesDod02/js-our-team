6a74416a106d00f791328056a62f51dfdfd5cf68
AlesDod02 <alessandro.dod@gmail.com>
2023-11-21 16:40:03 +0100

    working

--- js/main.js
+++ js/main.js
@@ -1,54 +1,87 @@
-let ourTeam = 
-[
-{
-    name: "Wayne",
-    surname: "Barnett",
-    role: "Founder & CEO",
-    picture: "wayne-barnett-founder-ceo.png"
-},
+let ourTeam =
+    [
+        {
+            name: "Wayne Barnett",
+            
+            role: "Founder & CEO",
+            picture: "./img/wayne-barnett-founder-ceo.jpg"
+        },
 
-{
-    name: "Angela",
-    surname: "Caroll",
-    role: "Chief Editor",
-    picture: "angela-caroll-chief-editor.png"
-},
+        {
+            name: "Angela Caroll",
+            
+            role: "Chief Editor",
+            picture: "./img/angela-caroll-chief-editor.jpg"
+        },
 
-{
-    name: "Walter",
-    surname: "Gordon",
-    role: "Office Manager",
-    picture: "walter-goldon-office-manager.png"
-},
+        {
+            name: "Walter Gordon",
+            
+            role: "Office Manager",
+            picture: "./img/walter-gordon-office-manager.jpg"
+        },
 
-{
-    name: "Angela",
-    surname: "Lopez",
-    role: "Social Media Manager",
-    picture: "angela-lopez-social-media-manager.png"
-},
+        {
+            name: "Angela Lopez",
+            
+            role: "Social Media Manager",
+            picture: "./img/angela-lopez-social-media-manager.jpg"
+        },
 
-{
-    name: "Scott",
-    surname: "Estrada",
-    role: "Developer",
-    picture: "scott-estrada-developer.png"
-},
+        {
+            name: "Scott Estrada",
+            
+            role: "Developer",
+            picture: "./img/scott-estrada-developer.jpg"
+        },
 
-{
-    name: "Barbara",
-    surname: "Ramos",
-    role: "Graphic Designer",
-    picture: "barbara-ramos-graphic-designer.png"
-}
-]
+        {
+            name: "Barbara Ramos",
+            
+            role: "Graphic Designer",
+            picture: "./img/barbara-ramos-graphic-designer.jpg"
+        }
+    ];
 
 console.log(ourTeam);
 
-for(let i = 0; i < ourTeam.length; i++){
+
+
+for (let i = 0; i < ourTeam.length; i++) {
     console.log("name:", ourTeam[i].name);
-    console.log("surname:", ourTeam[i].surname);
+    
     console.log("role:", ourTeam[i].role);
     console.log("picture:", ourTeam[i].picture);
     console.log("");
 }
+
+let card = document.getElementById("card")
+for (let i = 0; i < ourTeam.length; i++) {
+    
+    
+    
+    card.innerHTML += `
+    <div class="col-4  "> <div class="card" ">
+<img src="${ourTeam[i].picture}" class="card-img-top" alt="foto">
+<div class="card-body">
+  <h5 class="card-title">${ourTeam[i].name}</h5>
+  <p class="card-text">
+  ${ourTeam[i].role}</p>
+  
+</div>
+</div>
+</div>`
+
+
+    
+}
+
+
+/*for (let i = 0; i < ourTeam.length; i++) {
+   // document.getElementById("name").innerHTML = ourTeam[i].name;
+    //document.getElementById("surname").innerHTML = ourTeam[i].surname;
+    //document.getElementById("role").innerHTML = ourTeam[i].role;
+    //document.getElementById("picture").innerHTML = ourTeam[i].picture;
+    //document.getElementById("ourteam").innerHTML = ourTeam;
+
+}*/
